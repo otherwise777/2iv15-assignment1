f78f9b441abd50b3137475565332e20b57754a79
Grabot <SanderKools@gmail.com>
2015-05-15 02:19:45 +0200

    Runge-Kutta Method
    implemented Runge-Kutta method and think it works

--- ConsoleApplication1/Solver.cpp
+++ ConsoleApplication1/Solver.cpp
@@ -10,7 +10,7 @@ using namespace std;
 #define RAND (((rand()%2000)/1000.f)-1.f)
 void simulation_step( std::vector<Particle*> pVector, std::vector<Force*> forces, float dt )
 {
-	int solver = 2;
+	int solver = 3;
 
 	int i, size = pVector.size();
 
@@ -35,27 +35,28 @@ void simulation_step( std::vector<Particle*> pVector, std::vector<Force*> forces
 	}
 	else if (solver == 2)
 	{
-		static std::vector<Particle*> pVectorCopy1;
 		std::vector<Vec2f> posTemp;
-		pVectorCopy1 = pVector;
+
+		//save original position
 		for (i = 0; i < size; i++)
 		{
 			posTemp.push_back(pVector[i]->m_Position);
 		}
-
+		//apply forces for midpoint calculation
 		for (int i = 0; i < forces.size(); i++) {
 			forces[i]->apply();
 		}
-
-
+		//calculate midpoint position
 		for (i = 0; i < size; i++)
 		{
-			pVectorCopy1[i]->m_Velocity += ((pVectorCopy1[i]->m_Force * dt) / pVectorCopy1[i]->m_mass);
-			pVectorCopy1[i]->m_Position += pVectorCopy1[i]->m_Velocity * (dt / 2);
+			pVector[i]->m_Velocity += ((pVector[i]->m_Force * dt) / pVector[i]->m_mass);
+			pVector[i]->m_Position += pVector[i]->m_Velocity * (dt / 2);
 		}
+		//recalculate the force for the midpoint
 		for (int i = 0; i < forces.size(); i++) {
 			forces[i]->apply();
 		}
+		//use midpoint forces on the original position
 		for (i = 0; i < size; i++)
 		{
 			pVector[i]->m_Velocity += ((pVector[i]->m_Force * dt) / pVector[i]->m_mass);
@@ -64,22 +65,65 @@ void simulation_step( std::vector<Particle*> pVector, std::vector<Force*> forces
 	}
 	else if (solver == 3)
 	{
-		static std::vector<Particle*> pVectorCopy1;
-		static std::vector<Particle*> pVectorCopy2;
-		static std::vector<Particle*> pVectorCopy3;
-		static std::vector<Particle*> pVectorCopy4;
-		pVectorCopy1 = pVector;
-		pVectorCopy2 = pVector;
-		pVectorCopy3 = pVector;
-		pVectorCopy4 = pVector;
+		std::vector<Vec2f> posTemp;
+		std::vector<Vec2f> k1;
+		std::vector<Vec2f> k2;
+		std::vector<Vec2f> k3;
+		std::vector<Vec2f> k4;
 
+		//save original position and initialize the 4 k's
 		for (i = 0; i < size; i++)
 		{
-			pVectorCopy2[i]->m_Velocity += ((pVectorCopy2[i]->m_Force * dt) / pVectorCopy2[i]->m_mass);
-			pVectorCopy2[i]->m_Position += pVectorCopy2[i]->m_Velocity * (dt / 2);
+			posTemp.push_back(pVector[i]->m_Position);
+			k1.push_back(Vec2f(0.0, 0.0));
+			k2.push_back(Vec2f(0.0, 0.0));
+			k3.push_back(Vec2f(0.0, 0.0));
+			k4.push_back(Vec2f(0.0, 0.0));
 		}
-
-		pVector = pVectorCopy1;
+		//calculate forces
+		for (int i = 0; i < forces.size(); i++) {
+			forces[i]->apply();
+		}
+		//calculate midpoint position and save k1
+		for (i = 0; i < size; i++)
+		{
+			pVector[i]->m_Velocity += ((pVector[i]->m_Force * dt) / pVector[i]->m_mass);
+			k1[i] = pVector[i]->m_Velocity;
+			pVector[i]->m_Position += pVector[i]->m_Velocity * (dt / 2);
+		}
+		//recalculate the force for the midpoint k2
+		for (int i = 0; i < forces.size(); i++) {
+			forces[i]->apply();
+		}
+		//use midpoint forces to calculate k3 and save k2
+		for (i = 0; i < size; i++)
+		{
+			pVector[i]->m_Velocity += ((pVector[i]->m_Force * dt) / pVector[i]->m_mass);
+			k2[i] = pVector[i]->m_Velocity;
+			pVector[i]->m_Position = posTemp[i] + pVector[i]->m_Velocity * (dt / 2);
+		}
+		//recalculate the force for the midpoint
+		for (int i = 0; i < forces.size(); i++) {
+			forces[i]->apply();
+		}
+		//save k3 and set position for k4
+		for (i = 0; i < size; i++)
+		{
+			pVector[i]->m_Velocity += ((pVector[i]->m_Force * dt) / pVector[i]->m_mass);
+			k3[i] = pVector[i]->m_Velocity;
+			pVector[i]->m_Position = posTemp[i] + pVector[i]->m_Velocity * dt;
+		}
+		//recalculate the force for the midpoint
+		for (int i = 0; i < forces.size(); i++) {
+			forces[i]->apply();
+		}
+		//save k4 and use it to calculate the actual position
+		for (i = 0; i < size; i++)
+		{
+			pVector[i]->m_Velocity += ((pVector[i]->m_Force * dt) / pVector[i]->m_mass);
+			k4[i] = pVector[i]->m_Velocity;
+			pVector[i]->m_Position = posTemp[i] + (dt / 6)*(k1[i] + 2 * k2[i] + 2 * k3[i] + k4[i]);
+		}
 	}
 }
 
